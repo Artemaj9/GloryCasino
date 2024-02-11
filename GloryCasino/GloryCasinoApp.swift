283eb3e3046813ed05a219c3aa6f5f8da433440e
ArtemMoiseev <moiseev@physics.msu.ru>
2024-02-11 18:02:33 +0300

    Initial Commit

---
+++ GloryCasino/GloryCasinoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GloryCasinoApp.swift
+//  GloryCasino
+//
+//  Created by Artem on 11.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct GloryCasinoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

3acdba9fa2ee0a86f94b5bd63eee012c94c1c357
ArtemMoiseev <moiseev@physics.msu.ru>
2024-02-15 22:32:46 +0300

    before add end game

---
+++ GloryCasino/Components/RaysDust.swift
@@ -0,0 +1,18 @@
+//
+//  RaysDust.swift
+//  GloryCasino
+//
+//  Created by Artem on 15.02.2024.
+//
+
+import SwiftUI
+
+struct RaysDust: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RaysDust()
+}
---
+++ GloryCasino/Screens/WonSpins.swift
@@ -0,0 +1,18 @@
+//
+//  WonSpins.swift
+//  GloryCasino
+//
+//  Created by Artem on 15.02.2024.
+//
+
+import SwiftUI
+
+struct WonSpins: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WonSpins()
+}
